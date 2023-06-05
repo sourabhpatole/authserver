96785b7ad29ec0a0ea3016c7a72017ee4b711e15
sourabhpatole <sourabhpatole@outlook.com>
2023-06-05 12:42:12 +0530

    master

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const helmet = require("helmet");
+require("dotenv").config();
 const expressapp = require("express");
+const helmet = require("helmet");
 const app = expressapp();
 require("./db/conn");
 const router = require("./routes/router");
 const cors = require("cors");
 const cookiParser = require("cookie-parser");
 const userRoutes = require("./routes/userRouter");
-require("dotenv").config();
 const groupRoutes = require("./routes/groupRouter");
 const port = process.env.PORT || 5000;
 
