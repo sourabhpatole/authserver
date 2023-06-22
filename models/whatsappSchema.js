2241671de44a67fa1a0977081ad95eeb1bbfedfa
sourabhpatole <sourabhpatole@outlook.com>
2023-06-22 16:50:16 +0530

    master

--- models/whatsappSchema.js
+++ models/whatsappSchema.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
-const whatsappSchema = new mongoose.Schema({});
-const whatsappdb = new mongoose.model("whatsapp", whatsappSchema);
+const WhatsappSchema = new mongoose.Schema({});
+const whatsappdb = new mongoose.model("whatsapp", WhatsappSchema);
 module.exports = whatsappdb;
